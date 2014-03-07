1820f07dd35fd86352528fce5f1554bfb7545502
Sarath Lakshman <sarathlakshman@slynux.com>
2014-03-07 12:01:04 +0530

    commit validator: restore head commit after its operation

--- commit-tester/git-validate.sh
+++ commit-tester/git-validate.sh
@@ -9,9 +9,11 @@ fi
 rewind_no=$1
 cmd=$2
 
+> validation.txt
+
 head=`git log --oneline | head -1  | awk '{ print $1 }'`
 
-while [ $rewind_no -gt 0 ];
+while [ $rewind_no -ge 0 ];
 do
     commit="$head~$rewind_no"
     git checkout "$commit" > validation.txt 2>&1
@@ -20,9 +22,12 @@ do
     if [ $? -ne 0 ];
     then
         echo "FAILED"
+        git checkout $head > validation.txt 2>&1
         exit 2
     fi
     echo "OK"
     let rewind_no--
 done
+
+git checkout $head > validation.txt 2>&1
 
