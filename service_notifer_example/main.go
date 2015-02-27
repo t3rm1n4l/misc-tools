91573dd7b3a31b5f1e09392dc7807ed8c193657e
Sarath Lakshman <sarathlakshman@slynux.com>
2015-02-27 16:08:52 +0530

    Add services change notifier test script

---
+++ service_notifer_example/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/couchbase/cbauth"
+	"github.com/couchbase/indexing/secondary/common"
+	"os"
+	"time"
+)
+
+func client(id int, server string, cancel bool) {
+	sn, err := common.NewServicesChangeNotifier(common.ClusterUrl(server), "default")
+	fmt.Printf("client (%v)\n", id)
+	if err != nil {
+		fmt.Println("error %v", err)
+		os.Exit(1)
+	}
+
+	go func() {
+		if cancel {
+			time.Sleep(time.Second * 5)
+			sn.Close()
+		}
+	}()
+
+	for {
+		x, err := sn.Get()
+
+		if err != nil {
+			if err != nil {
+				fmt.Printf("Got error for client %v, %v", id, err)
+				os.Exit(1)
+			}
+		}
+		fmt.Printf("Got config for client %v\nconfig:%v\n", id, x)
+
+	}
+}
+
+func main() {
+	_, err := cbauth.InternalRetryDefaultInit("localhost:9000", "Administrator", "asdasd")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	for i := 0; i < 20; i++ {
+		go client(i, "localhost:9000", false)
+	}
+	for i := 20; i < 40; i++ {
+		go client(i, "localhost:9001", false)
+	}
+	for i := 40; i < 60; i++ {
+		go client(i, "localhost:9002", true)
+	}
+	time.Sleep(time.Hour)
+}
