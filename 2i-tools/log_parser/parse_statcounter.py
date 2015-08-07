31858c4899f2dc355a7445dd403a80de9a71aae1
Sarath Lakshman <sarathlakshman@slynux.com>
2015-08-07 18:22:11 +0530

    2i-tools: Add simple counter progress stats parser

---
+++ 2i-tools/log_parser/parse_statcounter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import json
+from dateutil import parser
+
+prevStats = {}
+last_time = None
+
+r = re.compile("^([^ ]*) .*PeriodicStats = ([^}]*})")
+if __name__ == '__main__':
+    f = open(sys.argv[1])
+    counter = sys.argv[2]
+    needTs = sys.argv[3]
+
+    for line in f.readlines():
+        if "Periodic" in line:
+            parsed = r.findall(line)
+            ts = parsed[0][0]
+            time = parser.parse(ts)
+            stats = parsed[0][1]
+            stats_json = json.loads(stats)
+            if prevStats.has_key(counter):
+                last_cntr = prevStats[counter]
+                curr_cntr = stats_json[counter]
+                d = time-last_time
+                secs = d.total_seconds()
+                diff = curr_cntr - last_cntr
+                if needTs == "yes":
+                    print ts, counter, diff, "=", str(diff/secs)+"/sec"
+                else:
+                    print counter, diff, "=", str(diff/secs)+"/sec"
+            prevStats = stats_json
+            last_time = time
