fd73d593ff07b79f5cb85813277ca22752f96b77
Sarath Lakshman <sarathlakshman@slynux.com>
2015-09-18 23:11:56 +0530

    Update tcp-proto

--- 2i-tests/tcp-proto/client/main.go
+++ 2i-tests/tcp-proto/client/main.go
@@ -8,49 +8,60 @@ import "strconv"
 import "io"
 import "sync"
 import "runtime"
+import "sync/atomic"
+
+var bytes uint64
 
 func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	var wg sync.WaitGroup
 
-	req := make([]byte, 50)
 	n, _ := strconv.Atoi(os.Args[1])
 	total := n
 	nconns, _ := strconv.Atoi(os.Args[2])
 	perconn := n / nconns
 
 	tcpAddr, _ := net.ResolveTCPAddr("tcp", "0:7777")
+	var conns []net.Conn
+	for i := 0; i < nconns; i++ {
+		conn, _ := net.DialTCP("tcp", nil, tcpAddr)
+		conns = append(conns, conn)
+	}
+
+	t0 := time.Now()
 
 	worker := func(wg *sync.WaitGroup, conn net.Conn, n int) {
 		defer wg.Done()
-		resp := make([]byte, 50)
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
+		resp := make([]byte, 200)
+		/*conn, err := net.DialTCP("tcp", nil, tcpAddr)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
+		*/
+		var err error
 		i := 0
 		for i = 1; i <= n; i++ {
+			req := make([]byte, 200)
 			_, err = conn.Write(req)
 			if err != nil {
 				fmt.Println(err)
 				break
 			}
 
-			io.ReadFull(conn, resp)
+			y, err := io.ReadFull(conn, resp)
+			if err != nil {
+				panic(err)
+			}
+			atomic.AddUint64(&bytes, uint64(y))
 		}
 	}
 
 	for i := 0; i < nconns; i++ {
 		var x int
 		wg.Add(1)
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
 
 		if i == nconns-1 {
 			x = n
@@ -59,13 +70,13 @@ func main() {
 			n -= perconn
 		}
 
-		go worker(&wg, conn, x)
+		go worker(&wg, conns[i], x)
 	}
-	t0 := time.Now()
 	wg.Wait()
 	dur := time.Now().Sub(t0)
 	fmt.Println(dur)
 
 	fmt.Printf("%d rows/sec\n", int(float64(total)/dur.Seconds()))
+	fmt.Println("bytes=", bytes)
 
 }
--- 2i-tests/tcp-proto/server/main.go
+++ 2i-tests/tcp-proto/server/main.go
@@ -2,28 +2,50 @@ package main
 
 import "net"
 import "runtime"
+import "strings"
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	res := make([]byte, 50)
+	res := make([]byte, 200)
 	ln, _ := net.Listen("tcp", ":7777")
 	for {
-		conn, _ := ln.Accept()
+		newconn, _ := ln.Accept()
 		//conn.(*net.TCPConn).SetWriteBuffer(16 * 1024)
 		//conn.(*net.TCPConn).SetReadBuffer(16 * 1024)
 
-		go func() {
-			b := make([]byte, 50)
-			for {
+		go func(conn net.Conn) {
+		loop:
+			for { // request
 
+				b := make([]byte, 200)
 				_, err := conn.Read(b)
 				if err != nil {
-					conn.Close()
-					return
+					//conn.Close()
+					if strings.Contains(err.Error(), "EOF") {
+						break loop
+					}
 				}
-				conn.Write(res)
+
+				ch1 := make(chan bool)
+				ch2 := make(chan bool)
+				donech := make(chan bool)
+
+				go func() {
+					<-ch1
+					go func() {
+						select {
+						case <-ch1:
+						case <-donech:
+						}
+					}()
+					conn.Write(res)
+					donech <- true
+					ch2 <- true
+				}()
+				ch1 <- true
+				<-ch2
 			}
-		}()
+		}(newconn)
 	}
 
 }
