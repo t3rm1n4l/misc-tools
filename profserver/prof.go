5e2cc9cf7576c41f626a335dc9dd6aa42ba7c59b
Sarath Lakshman <sarathlakshman@slynux.com>
2016-01-12 09:23:40 +0530

    Add mem stats for profserver

--- profserver/prof.go
+++ profserver/prof.go
@@ -2,9 +2,20 @@ package profserver
 
 import _ "net/http/pprof"
 import "net/http"
+import "runtime"
+import "encoding/json"
 
 func Init(Addr string) {
+	http.HandleFunc("/stats/mem", handleMemStats)
 	go func() {
 		http.ListenAndServe(Addr, nil)
 	}()
+}
+
+func handleMemStats(w http.ResponseWriter, r *http.Request) {
+	stats := new(runtime.MemStats)
+	runtime.ReadMemStats(stats)
+	bytes, _ := json.Marshal(stats)
+	w.WriteHeader(200)
+	w.Write(bytes)
 }
