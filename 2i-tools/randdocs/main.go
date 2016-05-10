84cd9a8d3355484f601a262df4163dd0076a07bb
Sarath Lakshman <sarathlakshman@slynux.com>
2016-05-10 10:33:42 +0530

    randdocs: Add support for arrays

--- 2i-tools/randdocs/main.go
+++ 2i-tools/randdocs/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "crypto/rand"
+import rnd "math/rand"
 import "github.com/couchbase/go-couchbase"
 import "os"
 import "strconv"
@@ -22,8 +23,8 @@ func randString(n int) string {
 
 func main() {
 
-	if len(os.Args) != 9 {
-		fmt.Println("./randdocs cluster:port bucket count field_sz junk_field_sz iterations threads doc_offset")
+	if len(os.Args) != 11 {
+		fmt.Println("./randdocs cluster:port bucket count docid_sz field_sz is_arr junk_field_sz iterations threads doc_offset")
 		os.Exit(1)
 	}
 
@@ -33,11 +34,13 @@ func main() {
 	b, _ := p.GetBucket(os.Args[2])
 
 	n, _ := strconv.Atoi(os.Args[3])
-	sz, _ := strconv.Atoi(os.Args[4])
-	junkSz, _ := strconv.Atoi(os.Args[5])
-	nitr, _ := strconv.Atoi(os.Args[6])
-	nthr, _ := strconv.Atoi(os.Args[7])
-	docOffset, _ := strconv.Atoi(os.Args[8])
+	docidSz, _ := strconv.Atoi(os.Args[4])
+	sz, _ := strconv.Atoi(os.Args[5])
+	is_arr := os.Args[6]
+	junkSz, _ := strconv.Atoi(os.Args[7])
+	nitr, _ := strconv.Atoi(os.Args[8])
+	nthr, _ := strconv.Atoi(os.Args[9])
+	docOffset, _ := strconv.Atoi(os.Args[10])
 
 	runtime.GOMAXPROCS(nthr)
 
@@ -49,8 +52,23 @@ func main() {
 				defer wg.Done()
 				for i := 0; i < n/nthr; i++ {
 					time.Sleep(20 * time.Microsecond)
-					docid := fmt.Sprintf("doc-%025d", i+offset+docOffset)
-					err := b.Set(docid, 0, map[string]interface{}{"name": randString(sz), "junk": fmt.Sprintf("%0*d", junkSz, 0)})
+					docid := fmt.Sprintf("doc-%0*d", docidSz, i+offset+docOffset)
+					value := make(map[string]interface{})
+					value["name"] = randString(sz)
+					if junkSz != 0 {
+						value["junk"] = fmt.Sprintf("%0*d", junkSz, 0)
+					}
+
+					if is_arr == "yes" {
+						seed := rnd.Int() % 1000000
+						value["arr"] = []int{seed, seed + 1, seed + 2, seed + 3, seed + 4, seed + 5, seed + 6, seed + 7, seed + 8, seed + 9}
+					}
+					_ = sz
+					_ = junkSz
+					err := b.Set(docid, 0, value)
+					//time.Sleep(time.Microsecond * 600)
+					//time.Sleep(time.Microsecond * 600)
+					//err := b.Delete(docid)
 					if err != nil {
 						fmt.Println(err)
 					}
